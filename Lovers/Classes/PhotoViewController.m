872a9a781b538e145aa08a52c27ac2c94782a734
Matt Di Pasquale <liveloveprosper@gmail.com>
2010-09-22 09:35:25 -0400

    fix photoView overlaySide origin.x; 70 => 120.0f

--- Lovers/Classes/PhotoViewController.m
+++ Lovers/Classes/PhotoViewController.m
@@ -75,11 +75,11 @@ NSMutableData *picData;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-	overlay = [[UIView alloc] initWithFrame:CGRectMake(0, 320, 320, 160 )];
+	overlay = [[UIView alloc] initWithFrame:CGRectMake(0.0f, 320.0f, 320.0f, 160.0f)];
 	overlay.backgroundColor = [UIColor clearColor];
 	//overlay.alpha = 0.5;
 	
-	UILabel *userAboutHead = [[UILabel alloc] initWithFrame:CGRectMake(25, 5, 280, 30)];
+	UILabel *userAboutHead = [[UILabel alloc] initWithFrame:CGRectMake(25.0f, 5.0f, 280.0f, 30.0f)];
 	userAboutHead.text = [targetUser headline];
 	userAboutHead.backgroundColor = [UIColor clearColor];
 	userAboutHead.shadowColor = [UIColor blackColor];
@@ -101,7 +101,7 @@ NSMutableData *picData;
 	[aboutLabel release];
 	[self.view addSubview:overlay];
 
-	overlaySide = [[UIView alloc] initWithFrame:CGRectMake(160, 10, 150, 310)];
+	overlaySide = [[UIView alloc] initWithFrame:CGRectMake(160.0f, 10.0f, 150.0f, 310.0f)];
 	overlaySide.backgroundColor = [UIColor clearColor];
 
 	// Lots of repetition here. Keep code DRY (Donot Repeat Yourself). Refactor with: 
@@ -109,14 +109,14 @@ NSMutableData *picData;
 	// Also, labals should be created & displayed only if corresponding value !- nil
 	// They should also be positioned vertically w/o gaps.
 	// So, the origin.y should be a variable and only incremented if value != nil
-	UILabel *locationLabel =   [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 150, 20)];
-	UILabel *lastOnlineLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, 150, 15)];
-	UILabel *ageLabel =        [[UILabel alloc] initWithFrame:CGRectMake(0, 50, 150, 15)];
-	UILabel *likesLabel =      [[UILabel alloc] initWithFrame:CGRectMake(0, 35, 150, 15)];
-	UILabel *heightLabel =     [[UILabel alloc] initWithFrame:CGRectMake(0, 65, 150, 15)];
-	UILabel *weightLabel =     [[UILabel alloc] initWithFrame:CGRectMake(0, 80, 150, 15)];
-	UILabel *ethnicityLabel =  [[UILabel alloc] initWithFrame:CGRectMake(0, 95, 150, 15)];
-	UILabel *sexLabel =        [[UILabel alloc] initWithFrame:CGRectMake(0, 110, 150, 15)];
+	UILabel *locationLabel =   [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 0.0f, 150.0f, 20.0f)];
+	UILabel *lastOnlineLabel = [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 20.0f, 150.0f, 15.0f)];
+	UILabel *ageLabel =        [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 50.0f, 150.0f, 15.0f)];
+	UILabel *likesLabel =      [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 35.0f, 150.0f, 15.0f)];
+	UILabel *heightLabel =     [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 65.0f, 150.0f, 15.0f)];
+	UILabel *weightLabel =     [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 80.0f, 150.0f, 15.0f)];
+	UILabel *ethnicityLabel =  [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 95.0f, 150.0f, 15.0f)];
+	UILabel *sexLabel =        [[UILabel alloc] initWithFrame:CGRectMake(0.0f, 110.0f, 150.0f, 15.0f)];
 	
 	locationLabel.backgroundColor =   [UIColor clearColor];
 	lastOnlineLabel.backgroundColor = [UIColor clearColor];
@@ -145,14 +145,14 @@ NSMutableData *picData;
 	ethnicityLabel.textColor =  [UIColor whiteColor];
 	sexLabel.textColor =        [UIColor whiteColor];
 	
-	locationLabel.font =   [UIFont systemFontOfSize:14.0];
-	lastOnlineLabel.font = [UIFont systemFontOfSize:12.0];
-	ageLabel.font =        [UIFont systemFontOfSize:12.0];
-	likesLabel.font =      [UIFont systemFontOfSize:12.0];
-	heightLabel.font =     [UIFont systemFontOfSize:12.0];
-	weightLabel.font =     [UIFont systemFontOfSize:12.0];
-	ethnicityLabel.font =  [UIFont systemFontOfSize:12.0];
-	sexLabel.font =        [UIFont systemFontOfSize:12.0];
+	locationLabel.font =   [UIFont systemFontOfSize:14.0f];
+	lastOnlineLabel.font = [UIFont systemFontOfSize:12.0f];
+	ageLabel.font =        [UIFont systemFontOfSize:12.0f];
+	likesLabel.font =      [UIFont systemFontOfSize:12.0f];
+	heightLabel.font =     [UIFont systemFontOfSize:12.0f];
+	weightLabel.font =     [UIFont systemFontOfSize:12.0f];
+	ethnicityLabel.font =  [UIFont systemFontOfSize:12.0f];
+	sexLabel.font =        [UIFont systemFontOfSize:12.0f];
 	
 	locationLabel.textAlignment =   UITextAlignmentRight;
 	lastOnlineLabel.textAlignment = UITextAlignmentRight;
@@ -188,26 +188,26 @@ NSMutableData *picData;
 	sexLabel.text =       [[Sexes objectAtIndex:[[targetUser sex] intValue]] capitalizedString];
 
 	UIButton * favorite = [UIButton buttonWithType:UIButtonTypeCustom];
-	favorite.frame = CGRectMake(70, 230, 30, 30);
+	favorite.frame = CGRectMake(120.0f, 230.0f, 30.0f, 30.0f);
 	[favorite setImage:[UIImage imageNamed:@"gold-star-2.jpg" ] forState:UIControlStateNormal];
 	[favorite addTarget:self action:@selector(favoriteAction:) forControlEvents:UIControlEventTouchUpInside];
 
-	UILabel *favoriteLabel = [[UILabel alloc] initWithFrame:CGRectMake(70, 260, 50, 10)];
+	UILabel *favoriteLabel = [[UILabel alloc] initWithFrame:CGRectMake(120.0f, 260.0f, 50.0f, 10.0f)];
 	favoriteLabel.text = @"favorite";
 	favoriteLabel.backgroundColor = [UIColor clearColor];
 	favoriteLabel.textColor = [UIColor whiteColor];
-	favoriteLabel.font =  [UIFont systemFontOfSize:10.0];
+	favoriteLabel.font =  [UIFont systemFontOfSize:10.0f];
 	
 	UIButton * block = [UIButton buttonWithType:UIButtonTypeCustom];
-	block.frame = CGRectMake(70, 270, 30, 30);
+	block.frame = CGRectMake(120.0f, 270.0f, 30.0f, 30.0f);
 	[block setImage:[UIImage imageNamed:@"BlockButton.png"] forState:UIControlStateNormal];
 	[block addTarget:self action:@selector(blockAction:) forControlEvents:UIControlEventTouchUpInside];
 	
-	UILabel *blockLabel = [[UILabel alloc] initWithFrame:CGRectMake(75, 300, 50, 10)];
+	UILabel *blockLabel = [[UILabel alloc] initWithFrame:CGRectMake(125.0f, 300.0f, 50.0f, 10.0f)];
 	blockLabel.text = @"block";
 	blockLabel.backgroundColor = [UIColor clearColor];
 	blockLabel.textColor = [UIColor whiteColor];
-	blockLabel.font =  [UIFont systemFontOfSize:10.0];
+	blockLabel.font =  [UIFont systemFontOfSize:10.0f];
 	
 //	moveMe.backgroundColor = [UIColor whiteColor];
 //	moveMe.titleLabel.text = @"move";
@@ -270,7 +270,7 @@ NSMutableData *picData;
 		[UIView setAnimationDuration:0.3f];
 		
 		//headerView.alpha = 0.0;
-		overlay.frame = CGRectMake(overlay.frame.origin.x, -160, overlay.frame.size.width, overlay.frame.size.height) ;
+		overlay.frame = CGRectMake(overlay.frame.origin.x, -160.0f, overlay.frame.size.width, overlay.frame.size.height) ;
 		//backBtn.alpha = 0.0;
 		
 		[UIView commitAnimations];
@@ -289,8 +289,8 @@ NSMutableData *picData;
 		[UIView setAnimationDuration:0.3f];
 		
 		//headerView.alpha = 0.0;
-		overlay.frame = CGRectMake(overlay.frame.origin.x, 640, overlay.frame.size.width, overlay.frame.size.height) ;
-		overlaySide.frame = CGRectMake(320, overlaySide.frame.origin.y, overlaySide.frame.size.width, overlaySide.frame.size.height) ;
+		overlay.frame = CGRectMake(overlay.frame.origin.x, 640.0f, overlay.frame.size.width, overlay.frame.size.height) ;
+		overlaySide.frame = CGRectMake(320.0f, overlaySide.frame.origin.y, overlaySide.frame.size.width, overlaySide.frame.size.height) ;
 		//backBtn.alpha = 0.0;
 		[[UIApplication sharedApplication] setStatusBarHidden:YES withAnimation:UIStatusBarAnimationFade];
 		[self.navigationController setNavigationBarHidden:YES];
@@ -304,8 +304,8 @@ NSMutableData *picData;
 			[UIView setAnimationDuration:0.3f];
 			
 			//headerView.alpha = 0.0;
-			overlay.frame = CGRectMake(overlay.frame.origin.x, 320, overlay.frame.size.width, overlay.frame.size.height) ;
-			overlaySide.frame = CGRectMake(210, overlaySide.frame.origin.y, overlaySide.frame.size.width, overlaySide.frame.size.height) ;
+			overlay.frame = CGRectMake(overlay.frame.origin.x, 320.0f, overlay.frame.size.width, overlay.frame.size.height) ;
+			overlaySide.frame = CGRectMake(160.0f, overlaySide.frame.origin.y, overlaySide.frame.size.width, overlaySide.frame.size.height) ;
 			//backBtn.alpha = 0.0;
 			[[UIApplication sharedApplication] setStatusBarHidden:NO withAnimation:UIStatusBarAnimationFade];
 			[self.navigationController setNavigationBarHidden:NO];
